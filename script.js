bed20738f1369c8894f8a268b246be86983aa387
wishba <wisnubayu34@gmail.com>
2022-10-02 01:13:11 +0700

    class notification

--- script.js
+++ script.js
@@ -7,21 +7,21 @@ function appendData(object) {
   for (const iterator of object) {
     console.log(iterator);
     let div = document.createElement("div")
+    // <div class="notification notification--unread">
+    div.className = "notification";
     div.innerHTML = `
-      <div class="notification notification--unread">
-        <div class="notification__avatar" style="content: url(${iterator.avatar});"></div>
-        <div class="notification__container">
-          <p class="notification__head">
-            <span class="notification__name">
-              <a href="#">${iterator.name}</a>
-            </span>
-            <span class="notification__do">${iterator.activityCategory}</span>
-            <span class="notification__post">
-              <a href="#">${iterator.activity}</a>
-            </span>
-          </p>
-          <p class="notification__time">${iterator.time}</p>
-        </div>
+      <div class="notification__avatar" style="content: url(${iterator.avatar});"></div>
+      <div class="notification__container">
+        <p class="notification__head">
+          <span class="notification__name">
+            <a href="#">${iterator.name}</a>
+          </span>
+          <span class="notification__do">${iterator.activityCategory}</span>
+          <span class="notification__post">
+            <a href="#">${iterator.activity}</a>
+          </span>
+        </p>
+        <p class="notification__time">${iterator.time}</p>
       </div>
     `;
     document.getElementById("notification").appendChild(div);
