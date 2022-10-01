2bf5e1986b741ff25cb7f7dcce98ab60c64e26f5
wishba <wisnubayu34@gmail.com>
2022-10-02 01:03:33 +0700

    un comment

--- script.js
+++ script.js
@@ -5,9 +5,24 @@ fetch("data.json")
 
 function appendData(object) {
   for (const iterator of object) {
+    console.log(iterator);
     let div = document.createElement("div")
     div.innerHTML = `
-      <p>${iterator.name}</p>
+      <div class="notification notification--unread">
+        <div class="notification__avatar" style="content: url(${iterator.avatar});"></div>
+        <div class="notification__container">
+          <p class="notification__head">
+            <span class="notification__name">
+              <a href="#">${iterator.name}</a>
+            </span>
+            <span class="notification__do">${iterator.activityCategory}</span>
+            <span class="notification__post">
+              <a href="#">${iterator.activity}</a>
+            </span>
+          </p>
+          <p class="notification__time">${iterator.time}</p>
+        </div>
+      </div>
     `;
     document.getElementById("notification").appendChild(div);
   }
