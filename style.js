90200be3cc8903b745171b13103292b142acd16f
wishba <wisnubayu34@gmail.com>
2022-10-04 09:49:25 +0700

    markAllRead

--- style.js
+++ style.js
@@ -1,3 +1,11 @@
 let unread = document.getElementsByClassName('notification--unread')
 let unreadCount = document.getElementById('totalUnread')
 unreadCount.innerHTML = unread.length
+
+function markAllRead() {
+  let notification = document.getElementsByClassName('notification')
+  for (const iterator of notification) {
+    iterator.classList.remove("notification--unread")
+  }
+  unreadCount.style.display = "none"
+}
